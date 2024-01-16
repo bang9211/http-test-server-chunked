fdf672babf2bf690fdd05611fc1113017d5b35c2
Han Young Tak[ 한영탁 ] <youngtak.han@linecorp.com>
2024-01-16 15:38:52 +0900

    First commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rc := http.NewResponseController(w)
+		rc.EnableFullDuplex()
+
+		fmt.Println(r.Proto, r.Method, r.URL.Path)
+		for k, v := range r.Header {
+			fmt.Println(k, v)
+		}
+		fmt.Println()
+		go readAsync(r)
+		writeAsyncWithScanln(w, rc)
+		r.Body.Close()
+
+		fmt.Println("============================================")
+	})
+
+	fmt.Println("Serving...")
+	log.Fatal(http.ListenAndServe(":8888", nil))
+}
+
+func readAsync(r *http.Request) {
+	buf := make([]byte, 1024)
+	for {
+		if r.Close {
+			fmt.Println("Connection closed")
+			break
+		}
+		n, err := r.Body.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("Read error =>", err.Error())
+				break
+			}
+			fmt.Print(n, " ", string(buf[:n]))
+			break
+		}
+		fmt.Print(n, " ", string(buf[:n]))
+	}
+	fmt.Println("Read done.")
+}
+
+func writeAsyncWithScanln(w io.Writer, wc *http.ResponseController) {
+	for {
+		var input string
+		fmt.Scanf("%s", &input)
+		if len(input) == 0 {
+			break
+		}
+		fmt.Fprint(w, input)
+		wc.Flush()
+	}
+	fmt.Println("Write done.")
+}
